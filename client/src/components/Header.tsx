bf77d508de6267b0b3cccb1cc9a5c74ad39e291e
Jibin <jibinbabu966@gmail.com>
2024-09-10 22:24:49 +0530

    server base

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">SignIn</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>SignIn</Button>
         </Link>
         <Navbar.Toggle />
       </div>
